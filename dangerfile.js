322ae35e3cea3b210e2c7674a9b373eba1ee4182
Breno Calazans <brenocalazans@gmail.com>
2020-03-21 18:43:29 -0300

    Update dangerfile

---
+++ dangerfile.js
@@ -0,0 +1,16 @@
+const {danger, warn, message } = require('danger')
+const { keepachangelog } = require('danger-plugin-keepachangelog')
+
+const pr = danger.github.pr
+
+// No PR is too small to include a description of why you made a change
+if (pr.body.length < 10) {
+  warn('Please include a description of your PR changes.');
+}
+
+keepachangelog({ changeVersion: false })
+
+// Add helpful message
+if (pr.author_association !== 'OWNER' && pr.maintainer_can_modify === false) {
+  message('Please consider [letting maintainers modify your pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/allowing-changes-to-a-pull-request-branch-created-from-a-fork) so they can help fix any issue.')
+}
