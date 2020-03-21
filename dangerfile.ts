c359579d0b1eee454377fe74ead741ca632e1feb
Breno Calazans <brenocalazans@gmail.com>
2020-03-21 18:42:06 -0300

    Update rules

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,12 +1,15 @@
-const {danger, warn} = require('danger')
+const {danger, warn, } = require('danger')
+
+const pr = danger.github.pr
 
 // No PR is too small to include a description of why you made a change
-if (danger.github && danger.github.pr.body.length < 10) {
+if (pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
+// No changelog entry
 const hasChangelog = danger.git.modified_files.includes("CHANGELOG.md")
-const isTrivial = danger.github && (danger.github.pr.body + danger.github.pr.title).includes("#trivial")
+const isTrivial = (pr.body + pr.title).includes("#trivial")
 
 if (!hasChangelog && !isTrivial) {
   warn("Please add a changelog entry for your changes.")
@@ -17,6 +20,6 @@ if (hasChangelog && !isTrivial) {
   console.log('hi')
 }
 
-if (danger.github && danger.github.pr.maintainer_can_modify === false) {
+if (pr.author_association !== 'OWNER' && pr.maintainer_can_modify === false) {
   warn('Please [let maintainers modify your pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/allowing-changes-to-a-pull-request-branch-created-from-a-fork) so they can help fix any issue.')
 }
