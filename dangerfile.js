a7867f5fa26e08565b70ab9fae1253552b5d5da7
Breno Calazans <brenocalazans@gmail.com>
2020-03-21 21:57:20 -0300

    Use plugin

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,5 @@
 const {danger, warn, message } = require('danger')
+const keepachangelog = require('danger-plugin-keepachangelog')
 
 const pr = danger.github.pr
 
@@ -7,59 +8,7 @@ if (pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
 }
 
-// No changelog entry
-const hasChangelog = danger.git.modified_files.includes("CHANGELOG.md")
-const isTrivial = (pr.body + pr.title).includes("#trivial")
-
-if (!hasChangelog && !isTrivial) {
-  error(`Thanks for your contribution!
-  We follow [keepachangelog](https://keepachangelog.com/) standards.
-
-  Please, change the file \`CHANGELOG.md\` adding a small summary of your changes under the \`[Unreleased]\` section. Example:
-
-  \`\`\`diff
-  ## [Unreleased]
-
-  + ### Fixed
-  + - Issue with image sizing in the SKU Selector.
-  \`\`\`
-
-  Possible types of changes:
-  - \`Added\` for new features.
-  - \`Changed\` for changes in existing functionality.
-  - \`Deprecated\` for soon-to-be removed features.
-  - \`Removed\` for now removed features.
-  - \`Fixed\` for any bug fixes.
-  - \`Security\` in case of vulnerabilities.
-  `)
-}
-
-// Check if the version was added as one of the CHANGELOG.md changes
-function getAddedChangelogLines() {
-  return danger.git.diffForFile('CHANGELOG.md')
-    .then((result) => {
-      console.log(result)
-      return result.diff
-        .split('\n')
-        .filter((line) => line.indexOf('+') === 0)
-        .map((line) => line.slice(1))
-    })
-}
-
-const newVersionLine = /## \[[0-9]/
-function findWrongChangelogEntries(changelog) {
-  return changelog.find((line) => newVersionLine.test(line))
-}
-
-if (hasChangelog && !isTrivial) {
-  getAddedChangelogLines()
-    .then(result => {
-      const wrongLine = findWrongChangelogEntries(result)
-      if (wrongLine) {
-        fail(`Please remove the line \`${wrongLine}\` from \`CHANGELOG.md\`\nThis will be automatically added once this PR is merged.`, 'CHANGELOG.md')
-      }
-    })
-}
+keepachangelog({ changeVersion: false })
 
 // Add helpful message
 if (pr.author_association !== 'OWNER' && pr.maintainer_can_modify === false) {
