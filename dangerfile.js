d05ef66e50a4f27457a9ac815ae8474cdc4660c7
Christian Kaisermann <christian@kaisermann.me>
2020-04-13 17:36:19 -0300

    Add @vtex/danger

--- dangerfile.js
+++ dangerfile.js
@@ -1,21 +1,3 @@
-const {danger, warn, message } = require('danger')
-const { keepachangelog } = require('danger-plugin-keepachangelog')
+const { assert } = require('@vtex/danger');
 
-const pr = danger.github.pr
-
-if (pr.user.type === 'Bot') {
-  warn('Skipping Danger checks: author of PR its a bot.')
-  return
-}
-
-// No PR is too small to include a description of why you made a change
-if (pr.body.length < 10) {
-  warn('Please include a description of your PR changes.');
-}
-
-keepachangelog({ changeVersion: false })
-
-// Add helpful message
-if (pr.author_association !== 'OWNER' && pr.maintainer_can_modify === false) {
-  message('Please consider [letting maintainers modify your pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/allowing-changes-to-a-pull-request-branch-created-from-a-fork) so they can help fix any issue.')
-}
+assert();
